2bd3d11d9b9ade9e85af9287f2fb62772ed5e613
Magdalena <magdalenahyra@wp.pl>
2022-10-18 21:30:25 +0200

    reducer used for add column and add card

--- src/components/CardForm/CardForm.js
+++ src/components/CardForm/CardForm.js
@@ -2,17 +2,21 @@ import styles from './CardForm.module.scss';
 import { useState } from 'react';
 import Button from './../Button/Button';
 import TextInput from './../TextInput/TextInput';
+import {useDispatch} from "react-redux";
 
 const CardForm = props => {
     const [title, setTitle] = useState('');
-    const handleSubmit = e => {
+
+    const dispatch = useDispatch();
+
+    const handleSubmit = columnId => e => {
         e.preventDefault();
-        props.action({ title: title }, props.columnId);
+        dispatch({ type: 'ADD_CARD', payload: { title, columnId } });
         setTitle('');
     };
 
 	return (
-        <form className={styles.cardForm} onSubmit={handleSubmit}>
+        <form className={styles.cardForm} onSubmit={handleSubmit(props.columnId)}>
             <TextInput value={title} onChange={e => setTitle(e.target.value)} />
             <Button>Add card</Button>
         </form>
