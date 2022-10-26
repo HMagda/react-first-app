9cfb2acd3ed0d613b2ff5f2078ef7e83bc6866ed
Magdalena <magdalenahyra@wp.pl>
2022-10-27 00:15:15 +0200

    Added stars for marking favoritie cards

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,8 +1,54 @@
 import styles from './Card.module.scss';
+import clsx from 'clsx';
+// import Button from '../Button/Button';
+import { FaRegStar, FaStar } from 'react-icons/fa';
+import {useState} from 'react';
+import {useDispatch} from "react-redux";
+import { toggleCardFavorite } from "../../redux/store";
+import { useEffect } from "react";
 
 const Card = props => {
+
+    const [isShownNotChosen, setIsShownNotChosen] = useState(true);
+    const [isShownChosen, setIsShownChosen] = useState(false);
+
+    // const [isFavorite, setIsFavorite] = useState(false);
+
+    // const dispatch = useDispatch();
+
+    // useEffect(() => {
+    //     dispatch(toggleCardFavorite(''));
+    //   }, [dispatch])
+    
+        const handleClick = (e) => {
+          e.preventDefault();
+          setIsShownChosen(current => !current);
+          setIsShownNotChosen(current => !current);
+
+
+
+        //   dispatch(toggleCardFavorite(!isFavorite));
+        //   setIsFavorite(!isFavorite);
+ 
+        }
+
+
     return (
-        <li className={styles.card}>{props.title}</li>
+    <>
+        <li className={styles.card}><p>{props.title}</p>
+
+        <div onClick={handleClick} className={styles.container}>
+                {isShownNotChosen && <FaRegStar className={styles.empty_star}/>}
+                {isShownChosen && <FaStar className={styles.full_star}/>}
+                 
+        </div>
+
+        
+        </li>
+        {/* <li className={clsx(styles.card, isFavorite && styles.isFavorite)}>{props.title}</li> */}
+        
+    </>
+
     );
 };
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,34 +1,39 @@
 import {createStore} from "redux";
 import initialState from "./initialState";
 import shortid from "shortid";
-import { strContains } from "../utils/strContains";
+import {strContains} from "../utils/strContains";
 
-export const addColumn = payload => ({ type: 'ADD_COLUMN', payload });
+export const addColumn = (payload) => ({type: "ADD_COLUMN", payload});
 
-export const addCard = payload => ({ type: 'ADD_CARD', payload });
+export const addCard = (payload) => ({type: "ADD_CARD", payload});
 
-export const updateSearchString = payload => ({ type: 'UPDATE_SEARCHSTRING', payload });
+export const updateSearchString = (payload) => ({
+  type: "UPDATE_SEARCHSTRING",
+  payload,
+});
 
-export const addList = payload => ({ type: 'ADD_LIST', payload });
+export const addList = (payload) => ({type: "ADD_LIST", payload});
 
 export const getFilteredCards = ({cards, searchString}, columnId) =>
   cards.filter(
     (card) =>
-      card.columnId === columnId &&
-      strContains(card.title, searchString)
+      card.columnId === columnId && strContains(card.title, searchString)
   );
 
 export const getAllColumns = (state) => state.columns;
 
-export const getListById = ({ lists }, listId) => lists.find(list => list.id === listId);
+export const getListById = ({lists}, listId) =>
+  lists.find((list) => list.id === listId);
 
 export const getColumnsByList = ({columns, searchString}, listId) =>
-columns.filter(
-    (column) =>
-    column.listId === listId 
-  );
+  columns.filter((column) => column.listId === listId);
 
-  export const getAllLists = (state) => state.lists;
+export const getAllLists = (state) => state.lists;
+
+export const toggleCardFavorite = (payload) => ({
+  type: "TOGGLE_CARD_FAVORITE",
+  payload,
+});
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -47,11 +52,20 @@ const reducer = (state, action) => {
         ...state,
         searchString: action.payload,
       };
-      case "ADD_LIST":
-        return {
-          ...state,
-          lists: [...state.lists, {...action.payload, id: shortid()}],
-        };
+    case "ADD_LIST":
+      return {
+        ...state,
+        lists: [...state.lists, {...action.payload, id: shortid()}],
+      };
+    case "TOGGLE_CARD_FAVORITE":
+      return {
+        ...state,
+        cards: state.cards.map((card) =>
+          card.id === action.payload
+            ? {...card, isFavorite: !card.isFavorite}
+            : card
+        ),
+      };
     default:
       return state;
   }
