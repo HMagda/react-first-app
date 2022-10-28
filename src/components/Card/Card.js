593718ec19183ee1840eab86f2abe37d37f846f4
Magdalena <magdalenahyra@wp.pl>
2022-10-28 13:36:02 +0200

    Added remove card option

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,6 +1,6 @@
 import styles from "./Card.module.scss";
 // import clsx from "clsx";
-import {FaRegStar, FaStar} from "react-icons/fa";
+import {FaRegStar, FaStar, FaTrash} from "react-icons/fa";
 import {useState} from "react";
 import {useDispatch} from "react-redux";
 import {toggleCardFavorite} from "../../redux/cardsRedux";
@@ -12,28 +12,56 @@ const Card = (props) => {
   const [isShownNotChosen, setIsShownNotChosen] = useState(true);
   const [isShownChosen, setIsShownChosen] = useState(false);
 
+  const [isNotDeleted, setIsNotDeleted] = useState(true);
+
   const dispatch = useDispatch();
 
-  const handleClick = (e) => {
+  const changeFavorites = (e) => {
     e.preventDefault();
     setIsShownChosen((current) => !current);
     setIsShownNotChosen((current) => !current);
 
+    // setIsNotDeleted(false);
+
     dispatch(toggleCardFavorite(props.id));
+  };
+
+  const deleteCard = (e) => {
+    e.preventDefault();
+
+    setIsNotDeleted(false);
+
+    // dispatch(toggleCardFavorite(props.id));
   };
 
   return (
     <>
-      <li className={styles.card}>
-        <p>{props.title}</p>
+      {isNotDeleted && (
+      <div>
+        <li className={styles.card}>
+          <p>{props.title}</p>
 
-        <div onClick={handleClick} className={styles.container}>
-          {isShownNotChosen && <FaRegStar className={styles.empty_star} />}
-          {isShownChosen && <FaStar className={styles.full_star} />}
-        </div>
-      </li>
-      {/* <li className={clsx(styles.card, isFavorite && styles.isFavorite)}>{props.title}</li> */}
+          <div className={styles.icon_wrapper}>
+            <div onClick={changeFavorites} className={styles.container}>
+              {isShownNotChosen && <FaRegStar className={styles.empty_star} />}
+              {isShownChosen && <FaStar className={styles.full_star} />}
+            </div>
+
+            <div onClick={deleteCard} className={styles.container}>
+              <FaTrash className={styles.trash}/>
+            </div>
+          </div>
+        </li>
+        {/* <li className={clsx(styles.card, isFavorite && styles.isFavorite)}>{props.title}</li> */}
+      </div>
+
+      )}
+
+
     </>
+
+
+
   );
 };
 
